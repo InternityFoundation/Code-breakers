c0ba3042632bef00ded19f30b7f1119fb25f9f30
kaushal gupta <36435529+kaushal-gupta@users.noreply.github.com>
2019-07-07 02:09:51 +0530

    second

--- detect.py
+++ detect.py
@@ -9,6 +9,9 @@ margin = 20
 frame_speed = 15
 tolerance = 20
 #
+
+List=['Bag','Comb','Watch','Gloves','Knife']
+item = 0
     
 if __name__=="__main__":
     root = tk.Tk()
@@ -20,6 +23,7 @@ if __name__=="__main__":
     vdots = 0
     vscan = []
     while dot < frame_height:
+        frame.create_oval(frame_width-margin, dot-2, frame_width-margin-2, dot, fill="green")
         vscan.append(( frame_width-margin, dot-2))
         dot += 15
         vdots += 1
@@ -38,14 +42,29 @@ if __name__=="__main__":
                 if  grey(*imagefile.getpixel(Wnew)) - Wp < 10:
                     return Wnew
 
+        #Detection
+        objects=[]
         W = 0
         E = 0
         N = [vdots,0]
         S = [0,0]
+        Wp=0
         for image in video.iter_data():
             imagefile=Image.fromarray(image)
             frame.itemconfig(screen, image=ImageTk.PhotoImage(imagefile))
             basepixel=grey(*imagefile.getpixel((frame_height-margin,5)))
+
+            for i in objects:
+                Wnew = findW( *i[:2] )
+                if Wnew:
+                    if Wnew[0] > margin:
+                        for j in i[2:]:
+                            frame.move(j, Wnew[0]-i[0][0], 0)
+                        i[0]=Wnew
+                    else:
+                        for j in i[2:]:
+                            frame.delete(j)
+                        objects.remove(i)
 
             obj = False
             for i in vscan:
@@ -69,6 +88,7 @@ if __name__=="__main__":
                     
             if obj == False and (N[1],S[1],W,E).count(0) == 0 :
                 focus = frame.create_rectangle(W[0], N[1][1], E[0], S[1][1], outline='darkblue')
+                objects.append( [W, Wp, focus] )                
                 W = 0
                 E = 0
                 N = [vdots, 0]
